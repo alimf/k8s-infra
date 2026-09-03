0bf19fa0d2a2d11a12d5365be664410d39c53aa9
alimf <alimf@gmail.com>
2026-09-03 19:07:08 +0600

    folder structure refactoring

--- terraform/environments/dev/aws/ec2/backend.tf
+++ terraform/environments/dev/aws/ec2/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "alimf-aws-terraform-993015604240-us-east-1-an"
+    bucket         = "k8s-infra-dev-us-east-1"
     key            = "dev/aws/terraform.tfstate"
     region         = "us-east-1"
     use_lockfile   = true
--- terraform/environments/dev/aws/k8s/backend.tf
+++ terraform/environments/dev/aws/k8s/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "alimf-aws-terraform-993015604240-us-east-1-an"
+    bucket       = "k8s-infra-dev-tfstate--use1-az4--x-s3"
     key          = "dev/k8s/terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
